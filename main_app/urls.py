b691f63b7be40ba907ce18faedf07a4a28e19615
pgf96 <pgfxx1@gmail.com>
2023-01-23 11:40:05 -0500

    add signup/login/logout

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('testpath/', views.testpath, name='testpath'),
-
-
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -10,3 +12,18 @@ def home(request):
 
 def testpath(request):
     return HttpResponse("hi")
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
