7a8a8c1e693c927c00b5689c7b2b5dd9fcee426a
14gko <your_email@example.com>
2023-01-24 12:09:26 -0500

    pull upstream

---
+++ main_app/forms.py
@@ -0,0 +1,18 @@
+# from django.forms import ModelForm
+# from .models import Dog
+# from django import forms
+
+# class DateInput(forms.DateInput):
+#     input_type = 'date'
+
+# # class VendaForm(forms.ModelForm):
+# #     class Meta:
+# #         model = Dog
+# #         fields = ('date_missing')
+# #         widgets = {'date_missing': DateInput()}
+
+# class EditForm(forms.ModelForm):
+#     class Meta:
+#         model = Dog
+#         fields = ['status', 'date_missing']
+
